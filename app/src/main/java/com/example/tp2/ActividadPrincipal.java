297fad00fb2176249d9dca56ecaf2a48f2a957e3
Android Studio <devandroid@ort>
2019-04-09 12:04:01 -0300

    primera version

---
+++ app/src/main/java/com/example/tp2/ActividadPrincipal.java
@@ -0,0 +1,18 @@
+package com.example.tp2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActividadPrincipal extends Activity {
+
+    public void Randomizar(View vista)//hacer un switch para cada id
+    {
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_principal);
+    }
+}
